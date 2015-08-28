b254c98884645b7377b1539e4dbe0b40c7d406c8
Christopher Evans <evanchri@Christophers-MacBook-Pro.local>
2015-08-27 23:19:05 -0600

    Initial Commit

---
+++ BallClock/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BallClock
+//
+//  Created by Christopher Evans on 8/27/15.
+//  Copyright (c) 2015 Morphosis Games. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
